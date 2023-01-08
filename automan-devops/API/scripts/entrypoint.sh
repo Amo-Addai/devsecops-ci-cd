ac2892920f90d86610ba5c4935a38f34e5a16539
mr_amoaddai <kwadwoamoad@gmail.com>
2023-01-08 11:26:16 +0000

    Delete automan-devops directory

--- automan-devops/API/scripts/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Activate virtual environment
-. /appenv/bin/activate
-
-## DOWNLOAD DEPENDENCIES FOR BUILDING
-
-# Run entrypoint.sh arguments  eg. "npm start"
-exec $@
--- automan-devops/API/scripts/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Activate virtual environment
-. /appenv/bin/activate
-
-## DOWNLOAD DEPENDENCIES FOR TESTING
-
-# Run test.sh arguments  eg. "npm start"
-exec $@
