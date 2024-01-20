ba5ccc8dea99feb430296c95ef07ea62018b9fec
Mr.Amo-Addai <kwadwoamoad@gmail.com>
2024-01-20 10:43:59 +0000

    .

--- docker-terraform-aws-gcp-azure/samples.tf
+++ docker-terraform-aws-gcp-azure/samples.tf
@@ -1,4 +1,4 @@
-# TODO: terraform init - setup terraform in current directory 
+# todo: terraform init - setup terraform in current directory 
 
 provider "aws" { // "provider" name
     version = "~> 2.0"
@@ -30,4 +30,49 @@ resource "aws_subnet" "new_resource_name" {
     availability_zone   = "eu-west-3a"
 }
 
-# TODO: terraform apply - execute config.tf files
+# todo: terraform apply - execute config.tf files; auto-gen's terraform.tfstate (json file with current state)
+
+resource "aws_vpc" "dev-vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name : "development" // key attribute Name; sets resource name in aws
+  }
+}
+
+resource "aws_vpc" "dev-vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    vpc-env : dev // custom attribute; for arbitrary key-value pairs
+  }
+}
+
+/** 
+
+# todo: commenting / removing these (with terraform apply) will also remove the resources from aws (if they existed in the previous .tf config)
+
+data "aws_vpc" "existing_vpc" {
+  default = true
+}
+
+resource "aws_subnet" "dev-subnet-2" {
+  vpc_id            = data.aws_vpc.existing_vpc.id
+  cidr_block        = "172.31.42.0/20"
+  availability_zone = "eu-west-3a"
+  tags = {
+    Name : "subnet-2-default"
+  }
+}
+
+# todo: terrform destroy - removes all config'd resources
+# OR: terraform destroy -target provider_resource.resource_name
+# eg: terraform destroy -target aws_subnet.dev-subnet-2
+
+# todo: it's best to remove resources, then terraform apply, so that .tf config files will correspond to the current state of cloud resources
+# using terraform detroy -target alone will still leave .tf config files inconsistent with the destroyed cloud resources in its current state
+
+*/
+
+# todo: terraform plan - preview list to actions to be executed to reach desired cloud state
+
+# todo: terraform apply -auto-approve - to auto-respond to t-apply's confirmation question
+
