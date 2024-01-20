01f61b8105f9b85935b9b53bed40f7cd983c92ce
Amo-Addai <kwadwoamoad@gmail.com>
2024-01-20 10:39:28 +0000

    .

--- docker-terraform-aws-gcp-azure/1/main.tf
+++ docker-terraform-aws-gcp-azure/1/main.tf
@@ -7,16 +7,20 @@ provider "aws" {
 
 resource "aws_vpc" "dev-vpc" {
     cidr_block  = "10.0.0.0/16"
-    tags        = {
-        Name: "development"
-    }
 }
 
 resource "aws_subnet" "dev-subnet-1" {
     vpc_id              = aws_vpc.dev-vpc.id
     cidr_block          = "10.0.10.0/24"
     availability_zone   = "eu-west-3a"
-    tags                = {
-        Name: "subnet-1-dev"
-    }
+}
+
+data "aws_vpc" "existing_vpc" {
+    default = true
+}
+
+resource "aws_subnet" "dev-subnet-2" {
+    vpc_id              = data.aws_vpc.existing_vpc.id
+    cidr_block          = "172.31.48.0/20"
+    availability_zone   = "eu-west-3a"
 }
--- docker-terraform-aws-gcp-azure/samples.tf
+++ docker-terraform-aws-gcp-azure/samples.tf
@@ -1,11 +1,12 @@
+# TODO: terraform init - setup terraform in current directory 
 
-provider "aws" {
+provider "aws" { // "provider" name
     version = "~> 2.0"
     region  = "us-east-1"
 }
 
-resource "aws_vpc" "example" {
-    cidr_block  = "10.0.0.0/16"
+resource "aws_vpc" "example" { // "provider_resource" "variable"
+    cidr_block  = "10.0.0.0/16" // IPv4 CIDR Block of 
 }
 
 provider "kubernetes" {
@@ -18,3 +19,15 @@ resource "kubernetes_namespace" "example" {
         name    = "sample-namespace"
     }
 }
+
+data "aws_vpc" "resource_name" { // query "provider_resource" for "resource_name" data
+    default = true
+}
+
+resource "aws_subnet" "new_resource_name" {
+    vpc_id              = data.aws_vpc.resource_name.id // get .id property of resource_name data from aws_vpc
+    cidr_block          = "172.31.48.0/20" // use sub-set IP from CIDR Block of existing resource_name
+    availability_zone   = "eu-west-3a"
+}
+
+# TODO: terraform apply - execute config.tf files
