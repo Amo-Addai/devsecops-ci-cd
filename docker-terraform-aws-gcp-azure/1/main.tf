b84204ee34f772e5ccb173ad13d49e2c702a7935
Mr.Amo-Addai <kwadwoamoad@gmail.com>
2024-01-20 15:15:44 +0000

    .

--- docker-terraform-aws-gcp-azure/1/main.tf
+++ docker-terraform-aws-gcp-azure/1/main.tf
@@ -1,26 +1,52 @@
-
 provider "aws" {
-    region      = "eu-west-3"
-    access_key  = "ACCESS_KEY"
-    secret_key  = "SECRET"
+  region     = "eu-west-3"
+  access_key = "ACCESS_KEY"
+  secret_key = "SECRET_KEY"
+}
+
+variable "subnet_cidr_block" {
+  description = "subnet cidr block"
 }
 
 resource "aws_vpc" "dev-vpc" {
-    cidr_block  = "10.0.0.0/16"
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name : "development",
+    vpc_enf : "dev"
+  }
 }
 
 resource "aws_subnet" "dev-subnet-1" {
-    vpc_id              = aws_vpc.dev-vpc.id
-    cidr_block          = "10.0.10.0/24"
-    availability_zone   = "eu-west-3a"
+  vpc_id            = aws_vpc.dev-vpc.id
+  cidr_block        = var.subnet_cidr_block
+  availability_zone = "eu-west-3a"
+  tags = {
+    Name : "subnet-1-dev"
+  }
 }
 
 data "aws_vpc" "existing_vpc" {
-    default = true
+  default = true
 }
 
 resource "aws_subnet" "dev-subnet-2" {
-    vpc_id              = data.aws_vpc.existing_vpc.id
-    cidr_block          = "172.31.48.0/20"
-    availability_zone   = "eu-west-3a"
+  vpc_id            = data.aws_vpc.existing_vpc.id
+  cidr_block        = "172.31.42.0/20"
+  availability_zone = "eu-west-3a"
+  tags = {
+    Name : "subnet-2-default"
+  }
 }
+
+output "dev-vpc-id" {
+  value = aws_vpc.dev-vpc.id
+}
+
+output "dev-subnet-1-id" {
+  value = aws_subnet.dev-subnet-1.id
+}
+
+output "dev-subnet-2-id" {
+  value = aws_subnet.dev-subnet-2.id
+}
+
