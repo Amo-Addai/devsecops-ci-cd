84461254016c2c67081dff24d68a6019d41bbd9d
Mr.Amo-Addai <kwadwoamoad@gmail.com>
2024-01-21 05:19:44 +0000

    .

--- terraform-aws-gcp-azure/2/main.tf
+++ terraform-aws-gcp-azure/2/main.tf
@@ -1,13 +1,22 @@
 provider "aws" {
   region     = "eu-west-3"
-  access_key = "ACCESS_KEY"
-  secret_key = "SECRET_KEY"
 }
+
+variable availability_zone {} // "eu-west-3a"
 
 variable "environment" {
   description = "current environment"
 }
 
+variable "cidr_blocks" {
+  description = "vpc cidr blocks"
+  type        = list(object({
+    cidr_block = string
+    name       = string
+  }))
+}
+
+/*
 variable "vpc_cidr_block" {
   description = "vpc cidr block"
   default     = "10.0.10.0/24"
@@ -17,46 +26,22 @@ variable "vpc_cidr_block" {
 variable "subnet_cidr_block" {
   description = "subnet cidr block"
 }
+*/
 
 resource "aws_vpc" "dev-vpc" {
-  cidr_block = var.vpc_cidr_block
+  cidr_block = var.cidr_blocks[0].cidr_block
   tags = {
-    Name : var.environment,
-    vpc_env : "dev"
+    Name : var.cidr_blocks[0].name,
+    vpc_env : var.environment
   }
 }
 
 resource "aws_subnet" "dev-subnet-1" {
   vpc_id            = aws_vpc.dev-vpc.id
-  cidr_block        = var.subnet_cidr_block
-  availability_zone = "eu-west-3a"
+  cidr_block        = var.cidr_blocks[1].cidr_block
+  availability_zone = var.availability_zone
   tags = {
-    Name : "subnet-1-dev"
+    Name : var.cidr_blocks[1].name
   }
-}
-
-data "aws_vpc" "existing_vpc" {
-  default = true
-}
-
-resource "aws_subnet" "dev-subnet-2" {
-  vpc_id            = data.aws_vpc.existing_vpc.id
-  cidr_block        = "172.31.42.0/20"
-  availability_zone = "eu-west-3a"
-  tags = {
-    Name : "subnet-2-default"
-  }
-}
-
-output "dev-vpc-id" {
-  value = aws_vpc.dev-vpc.id
-}
-
-output "dev-subnet-1-id" {
-  value = aws_subnet.dev-subnet-1.id
-}
-
-output "dev-subnet-2-id" {
-  value = aws_subnet.dev-subnet-2.id
 }
 
--- terraform-aws-gcp-azure/2/terraform.tfvars
+++ terraform-aws-gcp-azure/2/terraform.tfvars
@@ -1,3 +1,11 @@
-subnet_cidr_block = "10.0.40.0/24"
-vpc_cidr_block = "10.0.0.0/16"
 environment = "development"
+cidr_blocks = [
+    {
+        name = "development-vpc",
+        cidr_block = "10.0.0.0/16"
+    },
+    {
+        name = "subnet-1-dev",
+        cidr_block = "10.0.0.0/24"
+    }
+]
