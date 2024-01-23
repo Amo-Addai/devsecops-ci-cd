6dcaab0caa4a90c7d9bc6dfc6dbd9ed2d90e55e4
Mr.Amo-Addai <kwadwoamoad@gmail.com>
2024-01-23 17:16:58 +0000

    .

--- terraform-aws-gcp-azure/2/main.tf
+++ terraform-aws-gcp-azure/2/main.tf
@@ -1,47 +1,140 @@
 provider "aws" {
-  region     = "eu-west-3"
+  region = "eu-west-3"
 }
 
-variable availability_zone {} // "eu-west-3a"
+variable availability_zone {}
+variable env {}
+variable vpc_cidr_block {}
+variable subnet_cidr_block {}
+variable allowed_ips {}
+variable all_ips {}
+variable instance_type {}
 
-variable "environment" {
-  description = "current environment"
+resource "aws_vpc" "app-vpc" {
+  cidr_block = var.vpc_cidr_block
+  tags = {
+    Name : "${var.env}-vpc",
+  }
 }
 
-variable "cidr_blocks" {
-  description = "vpc cidr blocks"
-  type        = list(object({
-    cidr_block = string
-    name       = string
-  }))
+resource "aws_subnet" "app-subnet-1" {
+  vpc_id            = aws_vpc.app-vpc.id
+  cidr_block        = var.subnet_cidr_block
+  availability_zone = var.availability_zone
+  tags = {
+    Name : "${var.env}-subnet-1"
+  }
+}
+
+resource "aws_internet_gateway" "app-igw" {
+  vpc_id = aws_vpc.app-vpc.id
+  tags = {
+    Name : "${var.env}-igw"
+  }
+}
+
+resource "aws_default_route_table" "app-rtb-default" {
+  default_route_table_id = aws_vpc.app-vpc.default_route_table_id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.app-igw.id
+  }
+  tags = {
+    Name : "${var.env}-rtb-default"
+  }
+}
+
+resource "aws_route_table" "app-rtb" {
+  vpc_id = aws_vpc.app-vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.app-igw.id
+  }
+  tags = {
+    Name : "${var.env}-rtb"
+  }
+}
+
+resource "aws_route_table_association" "app-rtb-subnet" {
+  subnet_id      = aws_subnet.app-subnet-1.id
+  route_table_id = aws_route_table.app-rtb.id
+}
+
+resource "aws_default_security_group" "app-sg-default" {
+  vpc_id = aws_vpc.app-vpc.id
+  ingress {
+    from_port  = 22
+    to_port    = 22
+    protocol   = "tcp"
+    cidr_block = var.allowed_ips
+  }
+  ingress {
+    from_port  = 8080
+    to_port    = 8080
+    protocol   = "tcp"
+    cidr_block = var.all_ips
+  }
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_block      = var.all_ips
+    prefix_list_ids = []
+  }
+  tags = {
+    Name : "${var.env}-sg-default"
+  }
+}
+
+resource "aws_security_group" "app-sg" {
+  name   = "app-sg"
+  vpc_id = aws_vpc.app-vpc.id
+  ingress {
+    from_port  = 22
+    to_port    = 22
+    protocol   = "tcp"
+    cidr_block = var.allowed_ips
+  }
+  ingress {
+    from_port  = 8080
+    to_port    = 8080
+    protocol   = "tcp"
+    cidr_block = var.all_ips
+  }
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_block      = var.all_ips
+    prefix_list_ids = []
+  }
+  tags = {
+    Name : "${var.env}-sg"
+  }
+}
+
+data "aws_ami" "amazon-linux-image-latest" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
 }
 
 /*
-variable "vpc_cidr_block" {
-  description = "vpc cidr block"
-  default     = "10.0.10.0/24"
-  type        = string
-}
-
-variable "subnet_cidr_block" {
-  description = "subnet cidr block"
+output "aws_ami" { // output first, to cross-check the image info, specifically its id
+  value = data.aws_ami.amazon-linux-image-latest
 }
 */
 
-resource "aws_vpc" "dev-vpc" {
-  cidr_block = var.cidr_blocks[0].cidr_block
-  tags = {
-    Name : var.cidr_blocks[0].name,
-    vpc_env : var.environment
-  }
-}
-
-resource "aws_subnet" "dev-subnet-1" {
-  vpc_id            = aws_vpc.dev-vpc.id
-  cidr_block        = var.cidr_blocks[1].cidr_block
-  availability_zone = var.availability_zone
-  tags = {
-    Name : var.cidr_blocks[1].name
-  }
+resource "aws_instance" "app-server" {
+  ami           = data.aws_ami.amazon-linux-image-latest.id
+  instance_type = var.instance_type
+   
 }
 
--- terraform-aws-gcp-azure/2/terraform.tfvars
+++ terraform-aws-gcp-azure/2/terraform.tfvars
@@ -1,11 +1,8 @@
-environment = "development"
-cidr_blocks = [
-    {
-        name = "development-vpc",
-        cidr_block = "10.0.0.0/16"
-    },
-    {
-        name = "subnet-1-dev",
-        cidr_block = "10.0.0.0/24"
-    }
-]
+
+availability_zone = "eu-west-3b"
+env = "dev"
+vpc_cidr_block = "10.0.0.0/16"
+subnet_cidr_block = "10.0.10.0/24"
+all_ips = ["0.0.0.0/0"]
+allowed_ips = ["178.191.165.151/32"]
+instance_type = "t2.micro"
